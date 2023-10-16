951617a986a9a0602326f4aca985729738d3564b
rapiddev243 <147666165+rapiddev243@users.noreply.github.com>
2023-10-16 16:45:22 -0500

    Update migrate-db-8e884fe.sh

--- config/migrate-scripts/migrate-db-8e884fe.sh
+++ config/migrate-scripts/migrate-db-8e884fe.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-psql invidious kemal -c "ALTER TABLE channels DROP COLUMN subscribed"
-psql invidious kemal -c "ALTER TABLE channels ADD COLUMN subscribed timestamptz"
-psql invidious kemal -c "UPDATE channels SET subscribed = '2019-01-01 00:00:00+00'"
+psql youtube_remastered -c "ALTER TABLE channels DROP COLUMN subscribed"
+psql youtube_remastered -c "ALTER TABLE channels ADD COLUMN subscribed timestamptz"
+psql youtube_remastered -c "UPDATE channels SET subscribed = '2019-01-01 00:00:00+00'"
